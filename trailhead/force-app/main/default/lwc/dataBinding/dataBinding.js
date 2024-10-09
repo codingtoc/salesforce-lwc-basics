7c6467504203c05c9bc7d265c2adb22978f1a661
codingtoc <codingtoc@gmail.com>
2024-10-09 14:49:59 +0900

    SGTS - LWC - HTML Template Data Binding - Fetch Data Using Property With Input Change Event

--- trailhead/force-app/main/default/lwc/dataBinding/dataBinding.js
+++ trailhead/force-app/main/default/lwc/dataBinding/dataBinding.js
@@ -2,4 +2,8 @@ import { LightningElement } from "lwc";
 
 export default class DataBinding extends LightningElement {
   greeting = "Sanjay";
+
+  handleChange(event) {
+    this.greeting = event.target.value;
+  }
 }
