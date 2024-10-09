28c9e68a5b24cb67051711d1d05275b81ab34444
codingtoc <codingtoc@gmail.com>
2024-10-09 14:16:15 +0900

    SGTS - LWC - Wire Apex Class Method

---
+++ trailhead/force-app/main/default/lwc/wireApexDemo/wireApexDemo.js
@@ -0,0 +1,9 @@
+import { api, LightningElement, wire } from "lwc";
+import getContacts from "@salesforce/apex/ContactController.getContacts";
+
+export default class WireApexDemo extends LightningElement {
+  @api recordId;
+
+  @wire(getContacts, { accId: "$recordId" })
+  contacts; // data and error
+}
