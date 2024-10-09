30064329878047afd9dd395103dc5210421c236d
codingtoc <codingtoc@gmail.com>
2024-10-09 15:02:29 +0900

    SGTS - LWC - HTML Template Data Binding - Fetch Data Using Getters

--- trailhead/force-app/main/default/lwc/dataBinding/dataBinding.js
+++ trailhead/force-app/main/default/lwc/dataBinding/dataBinding.js
@@ -1,9 +1,20 @@
 import { LightningElement } from "lwc";
 
 export default class DataBinding extends LightningElement {
-  greeting = "Sanjay";
+  firstName = "";
+  lastName = "";
 
   handleChange(event) {
-    this.greeting = event.target.value;
+    const field = event.target.name;
+    if (field == "fname") {
+      this.firstName = event.target.value;
+    } else if (field == "lname") {
+      this.lastName = event.target.value;
+    }
+  }
+
+  // getter
+  get uppercasedFullName() {
+    return `${this.firstName} ${this.lastName}`.toUpperCase();
   }
 }
