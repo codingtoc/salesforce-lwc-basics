df42a21cb0f0a9a2bfdffd249ebf07cedf26f25e
codingtoc <codingtoc@gmail.com>
2024-10-09 15:11:48 +0900

    SGTS - LWC - HTML Template Data Binding - Fetch Data Using querySelector

--- trailhead/force-app/main/default/lwc/dataBinding/dataBinding.js
+++ trailhead/force-app/main/default/lwc/dataBinding/dataBinding.js
@@ -1,20 +1,9 @@
 import { LightningElement } from "lwc";
 
 export default class DataBinding extends LightningElement {
-  firstName = "";
-  lastName = "";
+  greeting = "Sanjay";
 
-  handleChange(event) {
-    const field = event.target.name;
-    if (field == "fname") {
-      this.firstName = event.target.value;
-    } else if (field == "lname") {
-      this.lastName = event.target.value;
-    }
-  }
-
-  // getter
-  get uppercasedFullName() {
-    return `${this.firstName} ${this.lastName}`.toUpperCase();
+  handleClick(event) {
+    this.greeting = this.template.querySelector("lightning-input").value;
   }
 }
