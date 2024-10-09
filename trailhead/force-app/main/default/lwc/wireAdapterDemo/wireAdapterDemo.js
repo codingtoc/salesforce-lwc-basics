7149d55212d223d0db9f05e1ab4723f5c5736605
codingtoc <codingtoc@gmail.com>
2024-10-09 13:30:52 +0900

    SGTS - LWC - LDS Wire Adapter - Using Object's Field Through String Array

--- trailhead/force-app/main/default/lwc/wireAdapterDemo/wireAdapterDemo.js
+++ trailhead/force-app/main/default/lwc/wireAdapterDemo/wireAdapterDemo.js
@@ -2,13 +2,14 @@ import { LightningElement, api, wire } from "lwc";
 import { getRecord, getFieldValue } from "lightning/uiRecordApi";
 // import NAME_FIELD from "@salesforce/schema/Account.Name";
 // import PHONE_FIELD from "@salesforce/schema/Account.Phone";
+const FIELDS = ["Account.Name", "Account.Phone"];
 
 export default class WireAdapterDemo extends LightningElement {
   @api recordId;
 
   @wire(getRecord, {
     recordId: "$recordId",
-    fields: ["Account.Name", "Account.Phone"]
+    fields: FIELDS
   })
   record; // data and error
 
