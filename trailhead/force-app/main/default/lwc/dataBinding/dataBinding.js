03aa2c044816013387b46b18209d2cd2007851f6
codingtoc <codingtoc@gmail.com>
2024-10-09 15:20:24 +0900

    SGTS - LWC - HTML Template Data Binding - Fetch Data Using querySelectorAll

--- trailhead/force-app/main/default/lwc/dataBinding/dataBinding.js
+++ trailhead/force-app/main/default/lwc/dataBinding/dataBinding.js
@@ -1,9 +1,18 @@
 import { LightningElement } from "lwc";
 
 export default class DataBinding extends LightningElement {
-  greeting = "Sanjay";
+  firstName = "";
+  lastName = "";
 
   handleClick(event) {
-    this.greeting = this.template.querySelector("lightning-input").value;
+    let input = this.template.querySelectorAll("lightning-input");
+
+    input.forEach((element) => {
+      if (element.name === "fname") {
+        this.firstName = element.value;
+      } else if (element.name === "lname") {
+        this.lastName = element.value;
+      }
+    });
   }
 }
