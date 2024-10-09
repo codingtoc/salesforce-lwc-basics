2ab6cbfa03c57cbf53a324d64777c980ad91b10d
codingtoc <codingtoc@gmail.com>
2024-10-09 14:26:11 +0900

    SGTS - LWC - Wire Apex Class Method - Property 대신 Function 이용

--- trailhead/force-app/main/default/lwc/wireApexDemo/wireApexDemo.js
+++ trailhead/force-app/main/default/lwc/wireApexDemo/wireApexDemo.js
@@ -3,7 +3,17 @@ import getContacts from "@salesforce/apex/ContactController.getContacts";
 
 export default class WireApexDemo extends LightningElement {
   @api recordId;
+  contacts;
+  error;
 
   @wire(getContacts, { accId: "$recordId" })
-  contacts; // data and error
+  wireContact({ error, data }) {
+    if (data) {
+      this.contacts = data;
+      this.error = undefined;
+    } else if (error) {
+      this.error = error;
+      this.contacts = undefined;
+    }
+  }
 }
